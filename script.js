1f5188a27276a7770e24fdc49dbb52a40686f5d1
gombermilan <gombermilan@gmail.com>
2025-01-13 19:35:32 +0100

    Kész

---
+++ script.js
@@ -0,0 +1,5 @@
+function kerdesfunct(){
+    let kerdesinput = document.getElementById("kerdes");
+    let kerdesoutput = document.getElementById("utolsokerdes");
+    kerdesoutput.innerHTML = "" + kerdesinput.value;
+}
